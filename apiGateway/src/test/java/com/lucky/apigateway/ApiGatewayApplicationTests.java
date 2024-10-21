2ddf0760ca3266cdba7f194777f637119a699d7f
박지훈 <pjh2996@naver.com>
2024-10-22 00:04:30 +0900

    config: gateway 모듈 생성

---
+++ apiGateway/src/test/java/com/lucky/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

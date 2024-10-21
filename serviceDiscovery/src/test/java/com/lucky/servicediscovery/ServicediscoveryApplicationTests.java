f2760a3ae48685f883f87c6e458299b4379768f8
박지훈 <pjh2996@naver.com>
2024-10-21 20:29:38 +0900

    feat: serviceDiscovery Eureka 추가

---
+++ serviceDiscovery/src/test/java/com/lucky/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

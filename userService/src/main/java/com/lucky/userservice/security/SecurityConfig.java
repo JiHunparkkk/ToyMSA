733e06ece4a486b694a9fc7072b3b85899aaafad
박지훈 <pjh2996@naver.com>
2024-10-21 23:41:44 +0900

    feat: 회원가입 기능 구현

---
+++ userService/src/main/java/com/lucky/userservice/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.lucky.userservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users/**", "/h2-console/**").permitAll()
+                )
+                .headers((headers) -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+                .build();
+    }
+}

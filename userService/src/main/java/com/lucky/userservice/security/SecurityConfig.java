64df5cfc06982bb6c80300ca287c9ccba0ec8268
박지훈 <pjh2996@naver.com>
2024-10-22 15:14:41 +0900

    feat: 로그인 기능 구현

--- userService/src/main/java/com/lucky/userservice/security/SecurityConfig.java
+++ userService/src/main/java/com/lucky/userservice/security/SecurityConfig.java
@@ -1,19 +1,27 @@
 package com.lucky.userservice.security;
 
+import com.lucky.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserService userService;
+    private Environment env;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -21,13 +29,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/user-service/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/**").access(
+                                new WebExpressionAuthorizationManager(
+                                        "hasIpAddress('localhost') or hasIpAddress('127.0.0.1')")
+                        )
+                        .anyRequest().authenticated()
                 )
                 .headers((headers) -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+                .addFilter(getAuthenticationFilter(authenticationManager(authenticationConfiguration)))
                 .build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+        return new AuthenticationFilter(authenticationManager, userService, env);
     }
 }
